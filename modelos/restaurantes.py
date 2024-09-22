d7f0d0a0e3f04c698f9ea126fbeae54a69746acd
Coutrinlu20 <amarallua20@gmail.com.br>
2024-09-22 20:21:41 -0300

    script atualizado

--- modelos/restaurantes.py
+++ modelos/restaurantes.py
@@ -3,6 +3,8 @@ class Restaurante :
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
 
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
@@ -13,5 +15,5 @@ restaurantes = [restaurante_praca, restaurante_pizza]
 # 
 # dir(): Retorna uma lista das propriedades e métodos dos objetos especificados
 # vars(): Retorna a propriedade __dict__ de um objeto
-print(vars(restaurante_praca))
-print(vars(restaurante_pizza))
+print(restaurante_praca)
+print(restaurante_pizza)
