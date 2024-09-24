9380bd97b1a168d8a224c5df094cd53157b0daac
Coutrinlu20 <amarallua20@gmail.com>
2024-09-24 17:48:46 -0300

    script atualizado

--- modelos/restaurantes.py
+++ modelos/restaurantes.py
@@ -1,10 +1,19 @@
 class Restaurante :
+
+    restaurantes = []
     def __init__(self, nome,categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self)
+
+
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
+    
+    def lista_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
@@ -12,8 +21,10 @@ restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-# 
+# parou na aula3 video 1
 # dir(): Retorna uma lista das propriedades e métodos dos objetos especificados
 # vars(): Retorna a propriedade __dict__ de um objeto
-print(restaurante_praca)
-print(restaurante_pizza)
+# print(restaurante_praca)
+# print(restaurante_pizza)
+
+Restaurante.lista_restaurantes()
